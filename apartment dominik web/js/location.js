04fda18877de7ee64d7257008ffb98c764d59cd2
Šimun <simun.hordov5@gmail.com>
2023-09-25 11:09:09 +0200

    Add files via upload

---
+++ apartment dominik web/js/location.js
@@ -0,0 +1,3 @@
+function myFunction(){
+    location.replace("https://goo.gl/maps/qNHKAXzCD8SJsf1J7")
+}
---
+++ apartment dominik web/js/vote.js
@@ -0,0 +1,13 @@
+function vote(){
+
+  var vote = prompt("My vote for this apartment: ");
+  if (vote < 5) {
+     console.log(" This apartment is okay");
+  } else if (vote <= 5) {
+      console.log("This is a very good apartment!");
+  }
+ 
+}
+
+
+
